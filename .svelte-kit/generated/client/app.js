20c2e89da52dc5243d73add82db4c56e0f5c8d3c
Daniel Burgess <dan-burgess@hotmail.com>
2024-02-18 18:33:53 +1030

    creating posts components

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -4,14 +4,17 @@ export const nodes = [
 	() => import('./nodes/0'),
 	() => import('./nodes/1'),
 	() => import('./nodes/2'),
-	() => import('./nodes/3')
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
-		"/": [2],
-		"/posts": [3]
+		"/": [3],
+		"/posts": [4,[2]],
+		"/posts/create": [5,[2]]
 	};
 
 export const hooks = {
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,1 +1,2 @@
 <h1>Post Page</h1>
+<p>you can <a href="/posts/create">+create</a> or edit your posts here.</p>
