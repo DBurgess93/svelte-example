78aaf511c8f993eea132eab0197887e40e646fec
Daniel Burgess <dan-burgess@hotmail.com>
2024-02-23 18:14:15 +1030

    Server side rendering - last commit was client side... my bad

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,17 @@
 <script lang="ts">
-	import type { Post } from '@prisma/client';
-	async function getPosts() {
-		const response = await fetch('api/posts');
-		const posts: Post[] = await response.json();
-		return posts;
-	}
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	$: ({ posts } = data);
 </script>
 
 <h1>Posts</h1>
+<p>Showing {posts.length} posts</p>
 
-{#await getPosts()}
-	<p>Loading...</p>
-{:then posts}
-	<p>Showing {posts.length} posts</p>
-	{#each posts as { slug, title }}
-		<ul>
-			<li>
-				<a href="/posts/{slug}">{title}</a>
-			</li>
-		</ul>
-	{/each}
-{:catch errror}
-	<p>{error.message}</p>
-{/await}
+{#each posts as { slug, title }}
+	<ul>
+		<li>
+			<a href="/posts/{slug}">{title}</a>
+		</li>
+	</ul>
+{/each}
