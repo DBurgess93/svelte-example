fc6c761abcc3f81b9993011f607e933f996ca4f9
Daniel Burgess <dan-burgess@hotmail.com>
2024-02-23 17:52:58 +1030

    Cache-Control headers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-	async function subscribe(event: Event) {
-		const form = event.target as HTMLFormElement;
-		const data = new FormData(form);
-
-		await fetch('/api/newsletter', {
-			method: 'POST',
-			body: data
-		});
+	import type { Post } from '@prisma/client';
+	async function getPosts() {
+		const response = await fetch('api/posts');
+		const posts: Post[] = await response.json();
+		return posts;
 	}
 </script>
 
-<h1>Newsletter</h1>
+<h1>Posts</h1>
 
-<form on:submit|preventDefault={subscribe}>
-	<input type="email" name="email" />
-	<button>Subscribe</button>
-</form>
+{#await getPosts()}
+	<p>Loading...</p>
+{:then posts}
+	<pre>
+  {JSON.stringify(posts, null, 2)}
+</pre>
+{:catch errror}
+	<p>{error.message}</p>
+{/await}
